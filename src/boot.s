8a01e659a5ba4170b3e33ddd6d86d387830dffa6
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2025-02-13 18:51:54 +0300

    Update boot.s

--- src/boot.s
+++ src/boot.s
@@ -1,26 +1,27 @@
 bits 32
 
-section .text:
-	align 4
-	dd 0x1BADB002
-	dd 0x00000000
-	dd -(0x1BADB002 + 0x00000000)
+section .multiboot
+align 4
+dd 0x1BADB002  
+dd 0x00000000  
+dd -(0x1BADB002 + 0x00000000) 
 
+section .text
 global start
 extern kmain
 
 start:
-	cli
-	mov esp, stack_space
-	call kmain
-	hlt
+    cli
+    mov esp, stack_space
+    call kmain
+    hlt
 
 HaltKernel:
-	cli
-	hlt
-	jmp HaltKernel
+    cli
+    hlt
+    jmp HaltKernel
 
 section .bss
+align 16
 resb 8192
 stack_space:
-	
